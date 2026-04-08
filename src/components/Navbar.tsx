d3279dd5447668d45048e529eda273dd7b9c5cc2
Shah Nawaz <shah40192nawaz@gmail.com>
2026-04-08 21:59:22 +0530

    favicon

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,5 @@
-import { Terminal } from 'lucide-react';
-import { Link, useNavigate } from 'react-router-dom';
-import { useAuth } from '../context/AuthContext';
+import { Link, useNavigate } from "react-router-dom";
+import { useAuth } from "../context/AuthContext";
 
 export function Navbar() {
   const navigate = useNavigate();
@@ -9,7 +8,7 @@ export function Navbar() {
   async function handleSignOut() {
     try {
       await signOut();
-      navigate('/');
+      navigate("/");
     } catch (e) {
       console.error(e);
     }
@@ -17,18 +16,30 @@ export function Navbar() {
 
   return (
     <nav className="fixed top-0 w-full z-50 bg-[#0e0e11]/85 backdrop-blur-xl shadow-2xl shadow-[#000000]/40 border-b border-[#48474b]/15">
-      <div className="flex items-center justify-between px-6 md:px-8 py-4 max-w-7xl mx-auto gap-4">
-        <Link className="flex items-center gap-3" to="/">
-          <Terminal className="text-primary w-6 h-6" />
-          <span className="text-2xl font-bold tracking-tighter text-on-surface font-headline">Intento</span>
+      <div className="flex items-center justify-between px-6 md:px-8 py-4 max-w-7xl mx-auto ">
+        <Link className="flex items-center gap-2" to="/">
+          <img
+            src="/png.png"
+            alt="Intento logo"
+            className="w-11 h-10 rounded-md object-contain"
+          />
+          <span className="text-2xl font-bold tracking-tighter text-on-surface font-headline">
+            Intento
+          </span>
         </Link>
 
         <div className="hidden md:flex items-center gap-8">
-          <Link className="text-secondary font-medium font-headline transition-colors" to="/#features">
+          <Link
+            className="text-secondary font-medium font-headline transition-colors"
+            to="/#features"
+          >
             Features
           </Link>
 
-          <Link className="text-on-surface/60 hover:text-on-surface transition-colors font-headline" to="/#security">
+          <Link
+            className="text-on-surface/60 hover:text-on-surface transition-colors font-headline"
+            to="/#security"
+          >
             Security
           </Link>
         </div>
